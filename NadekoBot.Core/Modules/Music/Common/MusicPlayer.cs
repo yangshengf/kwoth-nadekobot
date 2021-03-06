3b669fb6cc63180b3df66fba7a4b288600336d4f
yangshengf <31492969+yangshengf@users.noreply.github.com>
2021-03-05 17:14:16 -0800

    Refactor unsafe function to equivalent safe C# code

--- NadekoBot.Core/Modules/Music/Common/MusicPlayer.cs
+++ NadekoBot.Core/Modules/Music/Common/MusicPlayer.cs
@@ -585,22 +585,19 @@ namespace NadekoBot.Modules.Music.Common
                 return Queue.ToArray();
         }
 
-        //aidiakapi ftw
-        public static unsafe byte[] AdjustVolume(byte[] audioSamples, float volume)
+        // rewrite of the unsafe aidiakapi code
+        public static byte[] AdjustVolume(byte[] audioSamples, float volume)
         {
             if (Math.Abs(volume - 1f) < 0.0001f) return audioSamples;
 
             // 16-bit precision for the multiplication
-            var volumeFixed = (int)Math.Round(volume * 65536d);
+            int volumeFixed = (int) Math.Round(volume * 65536d);
 
-            var count = audioSamples.Length / 2;
-
-            fixed (byte* srcBytes = audioSamples)
-            {
-                var src = (short*)srcBytes;
-
-                for (var i = count; i != 0; i--, src++)
-                    *src = (short)(((*src) * volumeFixed) >> 16);
+            for (int i = 0; i + 1 < audioSamples.Length; i += 2) {
+                short duplet = (short) (audioSamples[i] | (audioSamples[i+1] << 8));
+                duplet = (short) (duplet * volumeFixed >> 16);
+                audioSamples[i] = (byte) duplet;
+                audioSamples[i+1] = (byte) (duplet >> 8);
             }
 
             return audioSamples;
